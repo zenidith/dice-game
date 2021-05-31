31be49677206e4acb55aa6e30f20305944f637a9
Kayne Sheenan <kaynesheenan+git@gmail.com>
2021-05-31 23:27:29 +1000

    added if/else operator at the end of the function for the logic of the game. If players get 20 or over, they win, then changing the .message to say that they won and to reveal the reset button

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-A dice game for Scrimba's Front End Dev Career Path
+A dice game for Scrimba's Front End Dev Career Path.
+
+The game is live and can be played [here!](https://zenidith.github.io/dice-game/)
 
 Happy Coding!
--- index.js
+++ index.js
@@ -19,17 +19,33 @@ rollBtn.addEventListener("click", function() {
     console.log(randomNumber)
 
     if (player1Turn) {
+        player1Score += randomNumber
+        player1Scoreboard.textContent = player1Score
         player1Dice.textContent = randomNumber
         player1Dice.classList.remove("active")
         player2Dice.classList.add("active")
         message.textContent = "Player 2 Turn"
 
     } else {
+        player2Score += randomNumber
+        player2Scoreboard.textContent = player2Score
         player2Dice.textContent = randomNumber
         player2Dice.classList.remove("active")
         player1Dice.classList.add("active")
         message.textContent = "Player 1 Turn"
 
     }
+
+    if (player1Score >= 20) {
+        message.textContent = "Player 1 has won! 🥳"
+        rollBtn.style.display = "none"
+        resetBtn.style.display = "block"
+    } else if (player2Score >= 20) {
+        message.textContent = "Player 2 has won! 🎉"
+        rollBtn.style.display = "none"
+        resetBtn.style.display = "block"
+    }
+    
+
     player1Turn = !player1Turn
 })
